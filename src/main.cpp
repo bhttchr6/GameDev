6dd1d45526cd6c49bcdf2f04a801fc73ab3322e4
Anurag Bhattacharyya <bhattacharyyanurag@gmail.com>
2025-01-31 21:37:49 -0800

    slightly boucing

--- src/main.cpp
+++ src/main.cpp
@@ -27,10 +27,12 @@ int main(int argc, char* argv[])
     float iniVel = 0.0;
     float time = 0.0;
     bool impact = false;
+    float localTime = 0.0;
    
     while (window.isOpen())
     {
-        time = time + dt;
+        time = time + localTime;
+        localTime = localTime + dt;
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
@@ -38,17 +40,25 @@ int main(int argc, char* argv[])
         }
         
         //Move the circle continuously
-        circle.setPosition(newPos(circle, time, iniVel));
+        circle.setPosition(newPos(circle, localTime, iniVel));
+        
         if(collison(circle, rectangle))
         {
             //throw std::runtime_error("collision detected");
             impact = true;
             // get final velocity of ball at time of collision
-            float finalVelocity = GetVelocity(iniVel, time);
+            float finalVelocity = GetVelocity(iniVel, localTime);
             std::cout << "velocity at collision = " << finalVelocity << std::endl;
 
             //The initial velocity of ball = -e * final velocity of ball
             iniVel = -1 * world::e * finalVelocity;
+
+            //correct ball position
+            sf::Vector2f posCurr = circle.getPosition();
+            circle.setPosition(sf::Vector2f(posCurr.x, posCurr.y - circle.getRadius()));
+
+            //revert local time to 0
+            localTime = 0.0 ;
 
             
 
