b0c122c458a6f4c12673118f8b2bc02afc673ba4
Anurag Bhattacharyya <bhattacharyyanurag@gmail.com>
2025-03-14 23:03:22 -0700

    more files

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,252 @@
-#include <stdio.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "camera.hpp"
+#include <../third/glm/glm.hpp>
+#include <../third/glm/gtc/matrix_transform.hpp>
+#include <../third/glm/gtc/type_ptr.hpp>
 #include <iostream>
-#include <SFML/Graphics.hpp>
-#include "utils.hpp"
+#include <vector>
+#include "objLoader.hpp"
 
+#include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
-int main(int argc, char* argv[])
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+int main()
 {
-    sf::RenderWindow window(sf::VideoMode({500, 500}), "Application Window!");
-    
-    // circle shape
-    sf::CircleShape circle(10.0f);
-    circle.setPosition(sf::Vector2f(10, 0));
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // rectangle shape
-    sf::RectangleShape rectangle;
-    rectangle.setSize(sf::Vector2f(300, 50));
-    rectangle.setPosition(sf::Vector2f(0, 400));
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    circle.setFillColor(sf::Color::Green);
-    rectangle.setFillColor(sf::Color::Red);
-
-    
-    int dir = 1;
-    float dt = 0.5;
-    float iniVel = 0.0;
-    float time = 0.0;
-    bool impact = false;
-    float localTime = 0.0;
-   
-    while (window.isOpen())
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
-        time = time + localTime;
-        localTime = localTime + dt;
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-        
-        //Move the circle continuously
-        circle.setPosition(newPos(circle, localTime, iniVel));
-        
-        if(collison(circle, rectangle))
-        {
-            //throw std::runtime_error("collision detected");
-            impact = true;
-            // get final velocity of ball at time of collision
-            float finalVelocity = GetVelocity(iniVel, localTime);
-            std::cout << "velocity at collision = " << finalVelocity << std::endl;
-
-            //The initial velocity of ball = -e * final velocity of ball
-            iniVel = -1 * world::e * finalVelocity;
-
-            //correct ball position
-            sf::Vector2f posCurr = circle.getPosition();
-            circle.setPosition(sf::Vector2f(posCurr.x, posCurr.y - circle.getRadius()));
-
-            //revert local time to 0
-            localTime = 0.0 ;
-
-            
-
-        }
-        window.clear();
-        window.draw(circle);
-        window.draw(rectangle);
-        window.display();
-
-        sf::sleep(sf::milliseconds(100)); // Slow down movement for visibility
-        
-    
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
     }
-    
-    
-    
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    /*
+    // ------------------------------------------------------------------
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    */
+   // load OBJ file
+   std::string filename = "../assets/shuttle.obj";
+   objLoader *objFile = new objLoader(filename);
+   std::vector<glm::vec3> vertexCoordinates = objFile->getVertexCoordinates();
+   std::vector<std::vector<int>> faces = objFile->getFaceIndices();
+
+   std::cout << "Loaded " << vertexCoordinates.size() << " vertices." << std::endl;
+   std::cout << "Loaded " << faces.size() << " faces." << std::endl;
+
+   // Evaluate the bounding box
+   objFile->evaulateBoundingBox();
+   boundingBox objBBox = objFile->getBoundingBox();
+
+   std::cout << "xMIn = " << objBBox.minBox.x<< " " << "yMin = " << objBBox.minBox.y<< " "  << "zMin = " << objBBox.minBox.z << std::endl;
+   std::cout << "xMax = " << objBBox.maxBox.x<< " "  << "yMax = " << objBBox.maxBox.y<< " "  << "zMax = " << objBBox.maxBox.z << std::endl;
+
+   // Scale the coordinates to be within -1 to 1
+
+   float vertices[3 * vertexCoordinates.size()];
+   int indices[3 * faces.size()];
+
+   for (unsigned int i = 0; i < vertexCoordinates.size(); i++)
+   {
+       glm::vec3 coords = vertexCoordinates[i];
+       /*
+       vertices[3 * i + 0] = coords.x;
+       vertices[3 * i + 1] = coords.y;
+       vertices[3 * i + 2] = coords.z;
+       */
+       // Apply the scaling formula
+        vertices[3 * i + 0] = 2.0f * (coords.x - objBBox.minBox.x) / (objBBox.maxBox.x - objBBox.minBox.x) - 1.0f;
+        vertices[3 * i + 1] = 2.0f * (coords.y - objBBox.minBox.y) / (objBBox.maxBox.y - objBBox.minBox.y) - 1.0f;
+        vertices[3 * i + 2] = 2.0f * (coords.z - objBBox.minBox.z) / (objBBox.maxBox.z - objBBox.minBox.z) - 1.0f;
+   }
+
+   for (unsigned int i = 0; i < faces.size(); i++)
+   {
+       std::vector<int> face = faces[i];
+       indices[3 * i + 0] = face[0] ;
+       indices[3 * i + 1] = face[1] ;
+       indices[3 * i + 2] = face[2] ;
+   }
+
+
+
+
+    int numVertices = faces.size() * 3;
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+
+    // uncomment this call to draw in wireframe polygons.
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, numVertices, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
     return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
